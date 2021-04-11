650508dc56a7d92208779dfd45cc3313954f70a9
Rodrigo Edson Fernandes <rodrigoedson@gmail.com>
2021-04-11 19:45:58 -0300

    Encrypt the credentials password

--- Project_1/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/CredentialController.java
+++ Project_1/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/CredentialController.java
@@ -3,6 +3,7 @@ package com.udacity.jwdnd.course1.cloudstorage.controller;
 import com.udacity.jwdnd.course1.cloudstorage.model.Credential;
 import com.udacity.jwdnd.course1.cloudstorage.model.User;
 import com.udacity.jwdnd.course1.cloudstorage.services.CredentialService;
+import com.udacity.jwdnd.course1.cloudstorage.services.EncryptionService;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -10,6 +11,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.security.SecureRandom;
+import java.util.Base64;
 import java.util.List;
 
 @Controller
@@ -17,16 +20,30 @@ import java.util.List;
 public class CredentialController {
 
     private CredentialService credentialService;
+    private EncryptionService encryptionService;
 
-    public CredentialController(CredentialService createService) {
-        this.credentialService = createService;
+    public CredentialController(CredentialService credentialService, EncryptionService encryptionService) {
+        this.credentialService = credentialService;
+        this.encryptionService = encryptionService;
     }
 
     @PostMapping
     public String insertUpdateCredential(@ModelAttribute Credential credential, Authentication auth, RedirectAttributes redirectAttributes){
         User user = (User) auth.getDetails();
-        credential.setUserId(user.getUserId());
 
+        //Encrypt the password
+        SecureRandom random = new SecureRandom();
+        byte[] key = new byte[16];
+        random.nextBytes(key);
+        String encodedKey = Base64.getEncoder().encodeToString(key);
+        String encryptedPassword = encryptionService.encryptValue(credential.getPassword(), encodedKey);
+
+        //update object
+        credential.setUserId(user.getUserId());
+        credential.setKey(encodedKey);
+        credential.setPassword(encryptedPassword);
+
+        //If ID exists then its an update action
         if (credential.getCredentialId() == null) {
             this.credentialService.createCredential(credential);
         }else{
@@ -34,7 +51,6 @@ public class CredentialController {
         }
 
         List<Credential> credentialsList = this.credentialService.getCredentialsByUserID(user.getUserId());
-
         redirectAttributes.addFlashAttribute("credentialsList", credentialsList);
         redirectAttributes.addFlashAttribute("activeTab", "credentials");
         return "redirect:/home";
--- Project_1/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/HomeController.java
+++ Project_1/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/HomeController.java
@@ -1,16 +1,41 @@
 package com.udacity.jwdnd.course1.cloudstorage.controller;
 
 
+import com.udacity.jwdnd.course1.cloudstorage.model.Credential;
+import com.udacity.jwdnd.course1.cloudstorage.model.Note;
+import com.udacity.jwdnd.course1.cloudstorage.model.User;
+import com.udacity.jwdnd.course1.cloudstorage.services.CredentialService;
+import com.udacity.jwdnd.course1.cloudstorage.services.NoteService;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/home")
 public class HomeController {
 
+    private CredentialService credentialService;
+    private NoteService noteService;
+
+    public HomeController(CredentialService credentialService, NoteService noteService) {
+        this.credentialService = credentialService;
+        this.noteService = noteService;
+    }
+
     @GetMapping()
-    public String getHomePage() {
+    public String getHomePage(Authentication auth, Model model) {
+        User user = (User) auth.getDetails();
+
+        List<Credential> credentialsList = this.credentialService.getCredentialsByUserID(user.getUserId());
+        List<Note> notesList = this.noteService.getNotesByUserID(user.getUserId());
+
+        model.addAttribute("credentialsList", credentialsList);
+        model.addAttribute("notesList", notesList);
+
         return "home";
     }
 
