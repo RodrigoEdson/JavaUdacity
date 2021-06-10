088287096b3c3eeb26c4924d45ca489cbf279426
Rodrigo Edson Fernandes <rodrigoedson@gmail.com>
2021-06-10 05:42:46 -0300

    Changig the API to a microservice using Spring data Rest

--- Project_2/pricing-service/src/main/java/com/udacity/pricing/repository/PriceRepository.java
+++ Project_2/pricing-service/src/main/java/com/udacity/pricing/repository/PriceRepository.java
@@ -1,13 +1,9 @@
 package com.udacity.pricing.repository;
 
 import com.udacity.pricing.domain.Price;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
+public interface PriceRepository extends CrudRepository<Price, Long> {
 
-public interface PriceRepository  extends CrudRepository<Price, Long> {
-
-    @Query("select p from Price p where p.vehicleId=:vehicleId")
-    Optional<Price> findPriceByVehicleId(Long vehicleId);
 }
